9e73d10fc07f6e54c9c6c6edc28b084b789f5ad7
Aarti Panchal <aarti.panchal@techdome.net.in>
2025-06-24 16:40:40 +0530

    update the workflows

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    // Redirect to a specified URL
+    const redirectTo = (url) => {
+      window.location.href = url;
+    };
+
+    redirectTo('https://techdome.io');
+  }, []);
+
+  return (
+    <div>
+      <h1>Redirecting...</h1>
+      {/* Add any additional content or components here */}
+    </div>
+  );
+}
+
+export default App;
